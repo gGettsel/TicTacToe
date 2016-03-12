6cad7d6834d3b700da1b8075a3afc013ba22b43c
gGettsel <g.gettsel@yandex.ru>
2016-03-12 21:32:17 +0500

    Added files via upload

---
+++ TicTac/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pole = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pole
+            // 
+            this.pole.AllowUserToAddRows = false;
+            this.pole.AllowUserToDeleteRows = false;
+            this.pole.AllowUserToResizeColumns = false;
+            this.pole.AllowUserToResizeRows = false;
+            this.pole.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.pole.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.pole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pole.ColumnHeadersVisible = false;
+            this.pole.Location = new System.Drawing.Point(0, 0);
+            this.pole.MultiSelect = false;
+            this.pole.Name = "pole";
+            this.pole.RowHeadersVisible = false;
+            this.pole.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.pole.Size = new System.Drawing.Size(410, 315);
+            this.pole.TabIndex = 0;
+            this.pole.TabStop = false;
+            this.pole.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 327);
+            this.Controls.Add(this.pole);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pole)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView pole;
+    }
+}
+
---
+++ TicTac/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        int CountColumn = 0;
+        int CountRows = 0;
+        bool who = true; // TRUE - X ; FALSE - O
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            DrawCell(10, 10);
+            
+        }
+
+        public void DrawCell(int count_x, int count_y)
+        {
+            for (int i = CountColumn; i < count_y; i++)
+            {
+                pole.Columns.Add("cell" + i, Convert.ToString(i));
+                pole.Columns[i].Width = 30;
+            }
+            pole.Rows.Add(count_y);
+            for (int j = CountRows; j < count_y; j++)
+                for (int i = CountColumn; i < count_x; i++)
+                    pole[i, j].Value = null;
+
+            CountColumn = count_x;
+        }
+
+        private int winHorizontal(int x, int y)
+        {
+            string whoByte = pole[x,y].Value.ToString();
+            bool flag = false;
+            int count = 0;
+            for (int temp = 1; temp < 5; temp++)
+            {
+                if (!flag)
+                {
+                    if ((pole[x + temp, y].Value == null) || (pole[x + temp, y].Value.ToString() != whoByte) || (x + temp > pole.ColumnCount))
+                    {
+                        flag = !flag;
+                        count = temp - 1;
+                        temp = 0;
+                    }
+                }
+                else
+                    if ((pole[x + temp, y].Value == null) || (pole[x - temp, y].Value.ToString() != whoByte) || (x - temp < 0) || pole[x - temp, y].Value == null)
+                    return count + temp;
+            }
+            return count;
+        }
+        private bool Win(int x, int y)
+        {
+            if (winHorizontal(x, y) == 5)
+                return true;
+            return false;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (who)
+                pole[e.ColumnIndex, e.RowIndex].Value = "X";
+            else
+                pole[e.ColumnIndex, e.RowIndex].Value = "O";
+            if (Win(e.ColumnIndex, e.RowIndex))
+                MessageBox.Show(who ? "Победили X" : "Победили O");
+            who = !who;
+        }
+    }
+}
